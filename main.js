fd04ee808394396dd76c37ae07f305646d53e647
Mateus Silva <mateus.ferreira@shopify.com>
2016-09-14 16:36:40 -0400

    Removing CSS and JS page injection

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
     var url = tabs[0].url;
     var re = /https:\/\/github.com\/[A-Za-z0-9\-\._]+\/[A-Za-z0-9\-\._]+\/pull\/[A-Za-z0-9\-\._]+\/files/g;
     if(url.match(re)) {
-      document.getElementById("container-success").classList += "show";
+      document.getElementById("container-success").classList.add("show");
     } else {
-      document.getElementById("container-error").classList += "show";
+      document.getElementById("container-error").classList.add("show");
     }
 });
 
@@ -15,7 +15,9 @@ clipboard.on('success', function(e) {
     console.info('Action:', e.action);
     console.info('Text:', e.text);
     console.info('Trigger:', e.trigger);
-    button.textContent = button.textContent + ' (Copied)';
+    if (button.textContent.indexOf('(Copied)') === -1) {
+      button.textContent = button.textContent + ' (Copied)';
+    }
 
     e.clearSelection();
 });
